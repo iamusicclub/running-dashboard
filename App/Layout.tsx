16dc08af1ad7666ca409d595b86b287420065e1e
iamusicclub <scottcee01@googlemail.com>
2026-03-11 16:19:38 +0000

    Create Layout.tsx

---
+++ App/Layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Running Dashboard",
+  description: "Running analytics dashboard"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
