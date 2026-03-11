909966277fc2648e7cb43f2cfabeb7871736cd4d
iamusicclub <scottcee01@googlemail.com>
2026-03-11 15:54:49 +0000

    Create Page.tsx

---
+++ App/Analysis/Page.tsx
@@ -0,0 +1,9 @@
+export default function Analysis() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Training Analysis</h1>
+
+      <p>Charts and insights about your training.</p>
+    </main>
+  )
+}
