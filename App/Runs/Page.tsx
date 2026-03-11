f6c4abba0c2898078ea90f3f752240ab5c6bbd88
iamusicclub <scottcee01@googlemail.com>
2026-03-11 15:53:44 +0000

    Create Page.tsx

---
+++ App/Runs/Page.tsx
@@ -0,0 +1,11 @@
+export default function Runs() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Your Runs</h1>
+
+      <p>Add and analyse your runs here.</p>
+
+      <button>Add Run</button>
+    </main>
+  )
+}
