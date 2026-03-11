0b2b931b337de70feef8e5d48a3c68417a58ad70
iamusicclub <scottcee01@googlemail.com>
2026-03-11 15:54:10 +0000

    Create Page.tsx

---
+++ App/Predictions/Page.tsx
@@ -0,0 +1,16 @@
+export default function Predictions() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Race Predictions</h1>
+
+      <p>Estimated times based on your training.</p>
+
+      <ul>
+        <li>5K: 20:30</li>
+        <li>10K: 43:10</li>
+        <li>Half Marathon: 1:36</li>
+        <li>Marathon: 3:25</li>
+      </ul>
+    </main>
+  )
+}
