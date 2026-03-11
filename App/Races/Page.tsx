4533430cc784c2a3c3bb8b80e2c93e8bc187ea4e
iamusicclub <scottcee01@googlemail.com>
2026-03-11 15:55:32 +0000

    Create Page.tsx

---
+++ App/Races/Page.tsx
@@ -0,0 +1,9 @@
+export default function Races() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Race Planner</h1>
+
+      <p>Plan upcoming races and track progress.</p>
+    </main>
+  )
+}
