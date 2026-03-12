95033e5806c8ba417c25c58a120b62bec82324e5
iamusicclub <scottcee01@googlemail.com>
2026-03-12 15:07:15 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { addDoc, collection, getDocs, orderBy, query } from "firebase/firestore";
-import { db } from "../../lib/firebase";
+import { db } from "../lib/firebase";
 
 type Run = {
   id: string;
